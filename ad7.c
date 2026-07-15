595c294804a06806af8b69d43eb6765578865be4
Masterofcode00 <themasterlion12@gmail.com>
2026-07-15 22:14:07 +0530

    Update ad7.c

--- ad7.c
+++ ad7.c
@@ -1,33 +1,36 @@
 #include <stdio.h>
+#define MAX 20
 int main() {
-    int n, m, w[20], p[20], i, j, cw = 0;
-    float r[20], profit = 0;
-    printf("Enter n and capacity: ");
-    scanf("%d%d", &n, &m);
-    printf("Enter weights:\n");
+    int n,w[MAX],p[MAX],x[MAX]={0},m,i,j,t,cw=0;
+    float r[MAX],tp=0,temp;
+    printf("Enter the number of objects: ");
+    scanf("%d",&n);
+    printf("Enter the objects' weights: ");
     for(i=0;i<n;i++) scanf("%d",&w[i]);
-    printf("Enter profits:\n");
-    for(i=0;i<n;i++) {
-        scanf("%d",&p[i]);
-        r[i] = (float)p[i]/w[i];
-    }
+    printf("Enter the objects' profits: ");
+    for(i=0;i<n;i++) scanf("%d",&p[i]);
+    printf("Enter the maximum capacity: ");
+    scanf("%d",&m);
+    for(i=0;i<n;i++) r[i]=(float)p[i]/w[i];
     for(i=0;i<n-1;i++)
         for(j=i+1;j<n;j++)
-            if(r[i] < r[j]) {
-                float t=r[i]; r[i]=r[j]; r[j]=t;
-                int x=w[i]; w[i]=w[j]; w[j]=x;
-                x=p[i]; p[i]=p[j]; p[j]=x;
+            if(r[i]<r[j]){
+                temp=r[i]; r[i]=r[j]; r[j]=temp;
+                t=w[i]; w[i]=w[j]; w[j]=t;
+                t=p[i]; p[i]=p[j]; p[j]=t;
             }
-    for(i=0;i<n;i++) {
-        if(cw + w[i] <= m) {
-            cw += w[i];
-            profit += p[i];
-        } else {
-            profit += (m-cw) * r[i];
+    for(i=0;i<n;i++){
+        if(cw+w[i]<=m){
+            x[i]=1;
+            cw+=w[i];
+            tp+=p[i];
+        }else{
+            tp+=(m-cw)*(float)p[i]/w[i];
             break;
-        }
-    }
-
-    printf("Max Profit = %.2f", profit);
+        }}
+    printf("\nOptimal solution for greedy method: %.1f",tp);
+    printf("\nSolution vector for greedy method: ");
+    for(i=0;i<n;i++)
+        printf("%d ",x[i]);
     return 0;
 }
