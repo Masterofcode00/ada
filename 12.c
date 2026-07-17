7c6cf21e30e26ba93e19f90a95de71d9a35c3a0d
Masterofcode00 <themasterlion12@gmail.com>
2026-07-17 08:23:13 +0530

    Update 12.c

--- 12.c
+++ 12.c
@@ -1,35 +1,41 @@
 #include <stdio.h>
 int b[10][10], n;
-int safe(int r,int c){
-    for(int i=0;i<c;i++)
+int safe(int r, int c){
+    int i, j;
+    for(i=0;i<c;i++)
         if(b[r][i]) return 0;
-    for(int i=r,j=c;i>=0&&j>=0;i--,j--)
+    for(i=r,j=c;i>=0&&j>=0;i--,j--)
         if(b[i][j]) return 0;
-    for(int i=r,j=c;i<n&&j>=0;i++,j--)
+    for(i=r,j=c;i<n&&j>=0;i++,j--)
         if(b[i][j]) return 0;
     return 1;
 }
 int queen(int c){
-    if(c==n) return 1;
-    for(int i=0;i<n;i++){
+    int i;
+    if(c==n)return 1;
+    for(i=0;i<n;i++){
         if(safe(i,c)){
             b[i][c]=1;
             if(queen(c+1)) return 1;
-            b[i][c]=0;
+            b[i][c]=0;     
         }}
     return 0;
 }
 int main(){
-    printf("Enter number of queens: ");
+    int i,j;
+    printf("Enter the number of queens: ");
     scanf("%d",&n);
+    for(i=0;i<n;i++)
+        for(j=0;j<n;j++)
+            b[i][j]=0;
     if(queen(0)){
-        printf("\nSolution:\n");
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++)
+        for(i=0;i<n;i++){
+            for(j=0;j<n;j++)
                 printf("%c ", b[i][j] ? 'Q' : '#');
             printf("\n");
         }}
-    else
-        printf("No Solution");
+    else{
+        printf("Solution does not exist");
+    }
     return 0;
 }
