83a8c2b1b7c4dfb4323318756410422bd043c2dd
Masterofcode00 <themasterlion12@gmail.com>
2026-07-19 11:23:02 +0530

    Update 2.c

--- 2.c
+++ 2.c
@@ -2,19 +2,19 @@
 int prim(int c[10][10],int n,int s){
     int v[10]={0},d[10],sum=0,min,u;
     int i,j;
-    for(i=0;i<n;i++) d[s]=c[s][i];
+    for(i=0;i<n;i++) d[i]=c[s][i];
     v[s]=1;
-    for(i=0;i<n;i++){
+    for(i=0;i<n-1;i++){
         min=999;
         for(j=0;j<n;j++){
-            if(!v[i]&&d[j]<min){
-                min=d[i];
+            if(!v[j]&&d[j]<min){
+                min=d[j];
                 u=j;
             }}
         v[u]=1;
         sum+=min;
         for(j=0;j<n;j++){
-            if(!v[i]&&c[u][j]<d[j]) d[j]=c[u][j];
+            if(!v[j]&&c[u][j]<d[j]) d[j]=c[u][j];
         }}
     return sum;
 }
@@ -29,6 +29,6 @@ int main(){
             scanf("%d",&c[i][j]);
     printf("Enter the source node:");
     scanf("%d",&s);
-    printf("\nTotal cost =%d\n") ,prim(c,n,s);
+    printf("\nTotal cost =%d\n",prim(c,n,s));
     return 0;
 }
